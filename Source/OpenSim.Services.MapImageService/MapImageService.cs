550f5475b3d07f36a4c049832d60c02a8c17f624
IainMcCracken <iain.mccracken@gmail.com>
2026-04-17 20:07:57 -0400

    Finished prettying up System.Services.MapImageService.MapImageService

--- Source/OpenSim.Services.MapImageService/MapImageService.cs
+++ Source/OpenSim.Services.MapImageService/MapImageService.cs
@@ -42,8 +42,11 @@ namespace OpenSim.Services.MapImageService;
 
 public class MapImageService : IMapImageService
 {
+    // Logging.
     private static readonly ILog m_log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
     private static readonly string LogHeader = "[MAP IMAGE SERVICE]";
+
+    // Image standards.
     private const int ZOOM_LEVELS = 8;
     private const int IMAGE_WIDTH = 256;
     private const int JPEG_QUALITY = 80;
@@ -140,7 +143,7 @@ public class MapImageService : IMapImageService
         }
 
         // If the write succeeded, we can queue this tile up for producing the relevant zoomed map tiles.
-        WorkQueue.Enqueue(x, y, scopeID);
+        ZoomTileWorkQueue.Enqueue(x, y, scopeID);
         return true;
     }
 
@@ -164,7 +167,7 @@ public class MapImageService : IMapImageService
         }
 
         // Queue up the deletion for regenerating zoomed map tiles.
-        WorkQueue.Enqueue(x, y, scopeID);
+        ZoomTileWorkQueue.Enqueue(x, y, scopeID);
         return true;
     }
 
@@ -255,10 +258,14 @@ public class MapImageService : IMapImageService
     }
 
     /// <summary>
-    /// Check if a given map tile file is a JPEG and is 256x256.
+    /// Check if a given map tile file is likely a JPEG and is 256x256.
     /// </summary>
+    /// <remarks>
+    /// <b>Note: This does not guarantee the file is a fully-well-formed JPEG!</b> It only checks it is likely to be JPEG, and has
+    /// enough metadata to correctly get the size, and the size is IMAGE_WIDTH.
+    /// </remarks>
     /// <param name="fileName">the file</param>
-    /// <returns>true if the tile is a 256x256 JPEG</returns>
+    /// <returns>true if the tile is likely a 256x256 JPEG</returns>
     private static bool IsMaptileJpeg(string fileName)
     {
         if (File.Exists(fileName))
@@ -282,12 +289,9 @@ public class MapImageService : IMapImageService
 
     #endregion
 
-
-
     #region Zoom tile creation
 
-
-    private sealed class WorkQueue
+    private sealed class ZoomTileWorkQueue
     {
         private static bool hasWork = false;
         private static bool triggered = false;
@@ -307,6 +311,16 @@ public class MapImageService : IMapImageService
             }
         }
 
+        /// <summary>
+        /// Public entrypoint to the Zoom Tile Work Queue.
+        /// </summary>
+        /// <remarks>
+        /// Enqueues a new incoming map tile, and starts a fuse. Every incoming tile resets the fuse, and when it runs out, the
+        /// gathered work batch is started.
+        /// </remarks>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="scopeID"></param>
         public static void Enqueue(int x, int y, UUID scopeID)
         {
             lock (m_queueLock)
@@ -330,6 +344,11 @@ public class MapImageService : IMapImageService
             }
         }
 
+        /// <summary>
+        /// We've had a couple seconds of silence since the last add/remove tile call, let's process any work.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="e"></param>
         private static void TriggerFired(Object o, ElapsedEventArgs e)
         {
             bool shouldFire = false;
@@ -345,36 +364,57 @@ public class MapImageService : IMapImageService
             }
         }
 
+        /// <summary>
+        /// Worker to process the current work queue. If there's been more incoming work, and the fuse has run out again (there's
+        /// another batch to do) we continue on.
+        /// </summary>
+        /// <param name="o"></param>
         private static void ZoomsWorker(Object o)
         {
             do
             {
                 lock (m_queueLock)
                 {
+                    // Snag the pending batch, set up to collect more.
                     (currentWork, pendingWork) = (pendingWork, currentWork);
                     pendingWork.Clear();
                     hasWork = false;
                     isWorking = true;
+                    triggered = false;
                 }
+                // Go do The Thing
                 DoScopes();
-            } while (pendingWork.Count != 0);
+            } while (triggered);
             isWorking = false;
         }
 
+        /// <summary>
+        /// Thin wrapper so we don't muddy the water with scopeIDs while zooming.
+        /// </summary>
         private static void DoScopes()
         {
             foreach (var key in currentWork.Keys)
             {
-                DoZooms(currentWork[key], key);
+                DoZooms(currentWork[key], GetScopeFolder(key));
             }
         }
 
-        private static void DoZooms(HashSet<TileInfo> levelOneWorkSet, UUID scopeID)
+        /// <summary>
+        /// Generate map tiles for zoom levels 2 and up.
+        /// </summary>
+        /// <param name="levelOneWorkSet"></param>
+        /// <param name="scopePath"></param>
+        private static void DoZooms(HashSet<TileInfo> levelOneWorkSet, string scopePath)
         {
-            string scopePath = GetScopeFolder(scopeID);
+            // A little commentary about "parent" and "child." The zoomed-in tiles are the children, the next zoomed-out tiles are
+            // the parents. Potentially four children per parent. Same as a tree data structure.
 
             HashSet<TileInfo> childSet = levelOneWorkSet;
             HashSet<TileInfo> parentSet;
+
+            // The processing is breadth-first, starting at level 1 and going down one level at a time. All tiles marked for
+            // consideration at a level are considered, potentially regenerating or deleting tiles at the next level. These are
+            // added to the set for the next level.
 
             for (int childLevel = 1; childLevel < ZOOM_LEVELS; childLevel++)
             {
@@ -382,6 +422,7 @@ public class MapImageService : IMapImageService
 
                 while (childSet.Count != 0)
                 {
+                    // Pick a tile, any tile! Don't care which.
                     HashSet<TileInfo>.Enumerator enumerator = childSet.GetEnumerator();
                     if (!enumerator.MoveNext())
                         continue;
@@ -389,28 +430,35 @@ public class MapImageService : IMapImageService
 
                     uint parentSize = 1u << (childLevel - 1);
                     uint mask = ~((parentSize << 1) - 1u);
-                    int px = (int)(((uint)childTile.x) & mask);
-                    int py = (int)(((uint)childTile.y) & mask);
                     int stride = (int)parentSize;
 
-                    using SKBitmap bottomLeft = GetExistingTileImage(childLevel, px, py, scopePath);
-                    using SKBitmap bottomRight = GetExistingTileImage(childLevel, px + stride, py, scopePath);
-                    using SKBitmap topLeft = GetExistingTileImage(childLevel, px, py + stride, scopePath);
-                    using SKBitmap topRight = GetExistingTileImage(childLevel, px + stride, py + stride, scopePath);
+                    // Get the parent tile's grid position.
+                    int px = (int)(((uint)childTile.x) & mask);
+                    int py = (int)(((uint)childTile.y) & mask);
 
+                    // Remove all four child tiles from consideration, we are considering all four now.
                     childSet.Remove(new TileInfo(px, py));
                     childSet.Remove(new TileInfo(px + stride, py));
                     childSet.Remove(new TileInfo(px, py + stride));
                     childSet.Remove(new TileInfo(px + stride, py + stride));
 
+                    // We are doing something to this file here. At least one child was either updated or removed, so this
+                    // parent tile will be changed or deleted as a result.
                     parentSet.Add(new TileInfo(px, py));
-
                     string parentFile = GetTileFileName(childLevel + 1, px, py, scopePath);
 
-                    bool regenerate = bottomLeft is not null || bottomRight is not null || topLeft is not null || topRight is not null;
+                    // Snag all child tiles that exist.
+                    using SKBitmap bottomLeft = GetExistingTileImage(childLevel, px, py, scopePath);
+                    using SKBitmap bottomRight = GetExistingTileImage(childLevel, px + stride, py, scopePath);
+                    using SKBitmap topLeft = GetExistingTileImage(childLevel, px, py + stride, scopePath);
+                    using SKBitmap topRight = GetExistingTileImage(childLevel, px + stride, py + stride, scopePath);
 
-                    if (regenerate)
+                    // If any child tile exists, we are updating this parent tile. If none exist, this parent is only ocean, so we
+                    // delete it.
+                    if (bottomLeft is not null || bottomRight is not null || topLeft is not null || topRight is not null)
                     {
+                        // We plop our potential four children onto a 512x512 of ocean, and resize it to 256x256, then save as
+                        // JPEG to file.
                         using SKBitmap tempBitmap = SkiaImageUtils.NewDefaultSKBitmap(512, 512);
                         using SKCanvas tempCanvas = new(tempBitmap);
                         tempCanvas.Clear(m_Watercolor);
@@ -420,7 +468,10 @@ public class MapImageService : IMapImageService
                         if (topLeft is not null) tempCanvas.DrawBitmap(topLeft, 0, 0);
                         if (topRight is not null) tempCanvas.DrawBitmap(topRight, IMAGE_WIDTH, 0);
 
-                        using SKBitmap newTile = tempBitmap.Resize(new SKImageInfo(IMAGE_WIDTH, IMAGE_WIDTH, SKColorType.Bgra8888, SKAlphaType.Opaque), new SKSamplingOptions(SKFilterMode.Linear, SKMipmapMode.None));
+                        using SKBitmap newTile
+                            = tempBitmap.Resize(
+                                new SKImageInfo(IMAGE_WIDTH, IMAGE_WIDTH, SKColorType.Bgra8888, SKAlphaType.Opaque),
+                                new SKSamplingOptions(SKFilterMode.Linear, SKMipmapMode.None));
                         using SKData newTileData = newTile.Encode(SKEncodedImageFormat.Jpeg, JPEG_QUALITY);
 
                         try
@@ -438,19 +489,18 @@ public class MapImageService : IMapImageService
                     }
                     else
                     {
-                        if (File.Exists(parentFile))
+                        // If there were no children, this parent tile is going to be all ocean, so we may as well delete it. We
+                        // know the file did exist. If there's no child tiles it's because at least one descendant was deleted.
+                        try
                         {
-                            try
+                            lock (m_FileAccessLock)
                             {
-                                lock (m_FileAccessLock)
-                                {
-                                    File.Delete(parentFile);
-                                }
+                                File.Delete(parentFile);
                             }
-                            catch (Exception e)
-                            {
-                                m_log.Warn($"{LogHeader}: Unable to delete all-water tile {parentFile}. Reason: {e.Message}");
-                            }
+                        }
+                        catch (Exception e)
+                        {
+                            m_log.Warn($"{LogHeader}: Unable to delete all-water tile {parentFile}. Reason: {e.Message}");
                         }
                     }
                 }
@@ -459,6 +509,15 @@ public class MapImageService : IMapImageService
             }
         }
 
+        /// <summary>
+        /// Get a bitmap from an existing file. This <b>should</b> be a 256x256 JPEG, unless someone's tampering with our maptiles
+        /// directory. because this module wrote them all.
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="path"></param>
+        /// <returns>A Skia bitmap</returns>
         private static SKBitmap GetExistingTileImage(int level, int x, int y, string path)
         {
             string fileName = GetTileFileName(level, x, y, path);
@@ -493,17 +552,7 @@ public class MapImageService : IMapImageService
             // The file did not exist, or we couldn't access it, or it wasn't a well-formed 256x256 JPEG.
             return null;
         }
-
     }
 }
 
-
-
 #endregion
-
-
-
-
-
-
-
